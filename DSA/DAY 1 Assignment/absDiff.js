292b276f449472097c5d4077d7a8203905851973
adnankhateeb <adnankhateeb21@gmail.com>
2022-04-06 15:26:59 +0530

    first commit

---
+++ DSA/DAY 1 Assignment/absDiff.js
@@ -0,0 +1,10 @@
+
+let arr = [1,2,3,4,5]
+let n = arr.length
+for(let i = 0; i<n-1; i++){
+    // let diff = Math.abs(arr[i] - arr[i+1])
+    console.log('arr[i]', arr[i],i);
+    console.log('arr[i+1]', arr[i+1],i)
+    // ans.push(arr[i] - arr[i+1])
+    // console.log(diff)
+}
